a394a3fafc73d0b764229be10126466c25f2fbc6
Mahmud Lukman <mistamann49@gmail.com>
2024-04-20 16:04:08 +0100

    travel model added

---
+++ server/models/Travel.ts
@@ -0,0 +1,39 @@
+import mongoose, { Document, Model, Schema } from 'mongoose';
+
+export interface ITravel extends Document {
+  title: string;
+  message: string;
+  name: string;
+  creator: string;
+  tags: string[];
+  selectedFile: {
+    public_id: string;
+    url: string;
+  };
+  likes: string[];
+  comments: string[];
+}
+
+const TravelSchema: Schema<ITravel> = new mongoose.Schema(
+  {
+    title: String,
+    message: String,
+    name: String,
+    creator: String,
+    tags: [String],
+    selectedFile: {
+      public_id: String,
+      url: String,
+    },
+    likes: { type: [String], default: [] },
+    comments: { type: [String], default: [] },
+  },
+  { timestamps: true }
+);
+
+const TravelModel: Model<ITravel> = mongoose.model<ITravel>(
+  'Travel',
+  TravelSchema
+);
+
+export default TravelModel;
