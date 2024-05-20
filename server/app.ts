acb3778f5309c86ad083c0996b68b85afe5e24a2
Mahmud Lukman <mistamann49@gmail.com>
2024-05-20 11:17:11 +0100

    cors update

--- server/app.ts
+++ server/app.ts
@@ -14,7 +14,19 @@ app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
 
 // cors => Cross Origin Resource Sharing
-app.use(cors());
+// app.use(
+//   cors({
+//     origin: ['http://localhost:5173'],
+//     credentials: true,
+//   })
+// );
+// Allow specific origin
+const corsOptions = {
+  origin: 'https://tapiya.netlify.app', // Replace with your client's domain
+  optionsSuccessStatus: 200, // For legacy browser support
+};
+
+app.use(cors(corsOptions));
 
 // routes
 app.use('/api/v1', userRouter);
