d8ec9d29027ee240aa0f3be733880eac93fa4eef
Mahmud Lukman <mistamann49@gmail.com>
2024-05-20 12:06:07 +0100

    another cors fix

--- server/app.ts
+++ server/app.ts
@@ -9,27 +9,27 @@ import travelRouter from './routes/travelRouter';
 
 // body parser
 app.use(express.json({ limit: '50mb' }));
+app.disable('x-powered-by')
 
 // cookie parser
 app.use(cookieParser());
 
 // cors => Cross Origin Resource Sharing
-// app.use(
-//   cors({
-//     origin: process.env.ORIGIN,
-//     credentials: true,
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-//     optionsSuccessStatus: 200,
-//   })
-// );
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    optionsSuccessStatus: 200,
+  })
+);
 
-app.use((req: Request, res: Response, next: NextFunction) => {
-  res.setHeader('Access-Control-Allow-Origin', process.env.ORIGIN as any)
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
-  res.setHeader('Access-Control-Allow-Headers', 'X-requested-With, Content-Type, Authorization')
-  next()
-})
+app.get('/cors-test', (req: Request, res: Response) => {
+  res.setHeader('Access-Control-Allow-Origin', process.env.ORIGIN as string);
+  res.status(200).json({ success: true, message: 'CORS test passed' });
+});
+
 
 // routes
 app.use('/api/v1', userRouter);
